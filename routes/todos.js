852b98823ee12c9b64a1918031594b9539b06b6a
SubhaKanji <subhakanji2000@gmail.com>
2024-04-02 11:23:13 +0530

    add route delete and get and update

--- routes/todos.js
+++ routes/todos.js
@@ -3,9 +3,16 @@ const router = express.Router();
 
 
 const { createTodo } = require("../controllers/createTodo");
+const {deleteTodo} = require ("../controllers/deleteTodo");
+const {getTodo} = require ("../controllers/getTodo");
+const {updateTodo} = require ("../controllers/updateTodo");
 
 
 router.post("/createTodo", createTodo);
+router.get("/getTodo",getTodo);
+router.delete("/deleteTodo",deleteTodo);
+router.put("/updateTodo",updateTodo);
+
 
 module.exports = router;
 
